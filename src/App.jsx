fcc3a2c66312ba281412ad090910ed26b00fffcd
rhona-tsc <rhona@thesupremecollective.co.uk>
2026-01-13 16:09:06 +0000

    changed act-presubmissions page from moderate to presubs

--- src/App.jsx
+++ src/App.jsx
@@ -178,7 +178,7 @@ const App = () => {
                     }
                   />
                   <Route
-                    path="/act-pre-submissions"
+                    path="/act-presubmissions"
                     element={<ActPreSubmissionsPage userRole={userRole} />}
                   />
                    <Route
