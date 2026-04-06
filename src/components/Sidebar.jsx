dba66b80322f489a3d029e7092b939258b458661
rhona-tsc <rhona@thesupremecollective.co.uk>
2026-04-06 18:07:42 +0100

    removed dupe

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -279,15 +279,7 @@ useEffect(() => {
         {normalize(userRole) === "agent" && (
           <>
 
-           {/* Deputy CTA */}
-        <NavLink
-          className="flex items-center gap-3 bg-black hover:bg-[#ff6667] border border-gray-300 border-r-0 px-3 py-2 rounded-l"
-          to={deputyCtaPath}
-          state={{ userRole, userFirstName }}
-        >
-          <img className="w-5 h-5" src={assets.deputy_icon} alt="" />
-          <p className="hidden md:block text-white">{deputyCtaLabel}</p>
-        </NavLink>
+      
 
                 <NavLink
           className="flex items-center gap-3 bg-black hover:bg-[#ff6667] border border-gray-300 border-r-0 px-3 py-2 rounded-l"
